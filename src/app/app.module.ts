5a567da7eba295c25cf372f985355f3c587d96ed
AngelE-Villa <71830766+AngelE-Villa@users.noreply.github.com>
2022-06-01 22:10:23 -0500

    Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistroVuelosComponent } from './componentes/registro-vuelos/registro-vuelos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from "@angular/router";
+import {MaterialModule} from "../material/material.module";
+
+
+const routes: Routes = [
+  {path:'', component:LoginComponent}
+]
 
 @NgModule({
   declarations: [
@@ -12,7 +20,10 @@ import { RegistroVuelosComponent } from './componentes/registro-vuelos/registro-
     RegistroVuelosComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
