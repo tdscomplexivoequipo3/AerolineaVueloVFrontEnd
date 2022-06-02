742b5e6571e7995e4d2d51616c4dbc4746ba56d9
Alejandro69-Ordo <72263584+Alejandro69-Ordo@users.noreply.github.com>
2022-06-02 07:16:55 -1200

    Login y Register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistroVuelosComponent } from './componentes/registro-vuelos/registro-vuelos.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {RouterModule, Routes} from "@angular/router";
 import {MaterialModule} from "../material/material.module";
-import { RegisterComponent } from './componentes/register/register.component';
+import { RegistroComponent } from './componentes/register/register.component';
 import {FormsModule} from "@angular/forms";
+import {RegistroUserComponent} from "./componentes/register/register.component";
 
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
-  {path:'registro', component:RegisterComponent}
+  {path:'registro', component:RegistroComponent}
 ]
 
 @NgModule({
@@ -21,7 +21,8 @@ const routes: Routes = [
     AppComponent,
     LoginComponent,
     RegistroVuelosComponent,
-    RegisterComponent
+    RegistroComponent,
+    RegistroUserComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
+
+export interface DialogData {
+  usuario: string;
+  clave: string;
+}
 
 @Component({
-  selector: 'app-register',
+  selector: 'app-registro',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegistroComponent {
 
-  constructor() { }
+  usuario?: string;
+   clave?: string;
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(RegistroUserComponent, {
+      width: '450px',
+      data: {name: this.usuario, animal: this.clave},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.usuario = result;
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-register',
+  templateUrl: 'dialog_users.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegistroUserComponent {
+  constructor(
+    public dialogRef: MatDialogRef<RegistroComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
