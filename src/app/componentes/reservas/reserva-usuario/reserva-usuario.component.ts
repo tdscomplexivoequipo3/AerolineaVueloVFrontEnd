504c66c411a1a17e90db974f318af7fe920e5570
Alejandro69-Ordo <72263584+Alejandro69-Ordo@users.noreply.github.com>
2022-06-03 09:07:40 -1200

    Login y Register

---
+++ src/app/componentes/reservas/reserva-usuario/reserva-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reserva-usuario',
+  templateUrl: './reserva-usuario.component.html',
+  styleUrls: ['./reserva-usuario.component.css']
+})
+export class ReservaUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
