f1b8f4d54c1daf7acfe655bd581fd859d28c7534
Alejandro69-Ordo <72263584+Alejandro69-Ordo@users.noreply.github.com>
2022-06-03 10:31:43 -1200

    Login y Register

---
+++ src/app/componentes/reservas/reserva-charter/reserva-charter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reserva-charter',
+  templateUrl: './reserva-charter.component.html',
+  styleUrls: ['./reserva-charter.component.css']
+})
+export class ReservaCharterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
